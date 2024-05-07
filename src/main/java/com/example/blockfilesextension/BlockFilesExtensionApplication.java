4c2261c1993038ffc7f2e32f4a10aa8dc752dae6
feelwjd <feelwjd@gmail.com>
2024-05-07 16:04:26 +0900

    build : first commit

---
+++ src/main/java/com/example/blockfilesextension/BlockFilesExtensionApplication.java
@@ -0,0 +1,13 @@
+package com.example.blockfilesextension;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockFilesExtensionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockFilesExtensionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/blockfilesextension/BlockFilesExtensionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blockfilesextension;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockFilesExtensionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
